5e08f640b9e2a111270052e06cc435788ccdcb86
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-06-30 00:12:30 +0700

    TabBarController added

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Konstantin Bolgar-Danchenko on 29.06.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
