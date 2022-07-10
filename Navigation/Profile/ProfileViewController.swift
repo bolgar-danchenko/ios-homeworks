2c7967f2c8495901ccecad37c710983c380d94fd
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-07-10 23:48:14 +0700

    homework 2.2 done

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,23 +9,57 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    private lazy var profileHeaderView = ProfileHeaderView()
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let view = ProfileHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private lazy var newButton: UIButton = {
+        let newButton = UIButton(type: .system)
+        newButton.backgroundColor = .systemBlue
+        newButton.setTitle("New Button", for: .normal)
+        newButton.setTitleColor(.white, for: .normal)
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        return newButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
+        setup()
+        setupSubview()
+        setupConstraints()
         
         let keyboard = UITapGestureRecognizer(target: self, action: #selector(self.typing))
         view.addGestureRecognizer(keyboard)
     }
     
-    override func viewWillLayoutSubviews() {
-        profileHeaderView.frame = view.frame
-    }
-    
     @objc private func typing() {
         view.endEditing(true)
     }
-}
 
+    private func setup() {
+        view.backgroundColor = .lightGray
+    }
+
+    private func setupSubview() {
+        view.addSubview(profileHeaderView)
+        view.addSubview(newButton)
+    }
+
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            profileHeaderView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            profileHeaderView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+
+            newButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            newButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+            newButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+            newButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
